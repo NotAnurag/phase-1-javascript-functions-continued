202c4033cfe3ab7b396c435116527807d5ac01ff
Anurag <anuragm2019@gmail.com>
2021-05-05 20:50:04 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(stuff='go to the office') {
+    return `This Monday, I will ${stuff}.`;
+}
+
+function wrapAdjective(sign='*') {
+    return function(parameter='special') {
+        return `You are ${sign}${parameter}${sign}!`;
+    }
+}
